94da74c7e8a084f363ab9d1ed2bc8d8290526bcf
Óscar <oscar.serrano91@gmail.com>
2025-02-19 13:25:42 +0100

    controladores, .env, fin ejemplo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { PORT } from './config.js';
 import router from './routes/users.routes.js';
+import morgan from 'morgan';
 
 const app = express(); 
 
-app.use(express.json()); 
+app.use(morgan('dev')); //Con morgan podemos ver peticiones que revibe el back por consola 
+app.use(express.json()); //Para entender las peticiones json 
 app.use(router); 
 
 app.listen(PORT);
